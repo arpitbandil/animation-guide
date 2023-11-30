9749f2e5453178dca9c416dfb51ab4c27ef69028
Arpit Bandil <arpit.bandil@cybg.com>
2023-11-30 16:55:27 +0530

    Working Minimum

--- app/src/main/java/com/absoft/rotateanimation/MainActivity.kt
+++ app/src/main/java/com/absoft/rotateanimation/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.absoft.rotateanimation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.absoft.rotateanimation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+    }
+
+    private var frontDegree = 0
+    private var backDegree = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.ivFrontDown.setOnClickListener(this)
+        binding.ivFrontUp.setOnClickListener(this)
+        binding.ivEndUp.setOnClickListener(this)
+        binding.ivEndDown.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.iv_front_up -> {
+                frontDegree += 10
+                binding.view1.animate().setDuration(ANIM_DURATION).rotation(frontDegree.toFloat())
+                handleFrontButtons()
+            }
+            R.id.iv_front_down -> {
+                frontDegree -= 10
+                binding.view1.animate().setDuration(ANIM_DURATION).rotation(frontDegree.toFloat())
+                handleFrontButtons()
+            }
+            R.id.iv_end_up -> {
+                backDegree += 10
+                binding.view3.animate().setDuration(ANIM_DURATION).rotation(-backDegree.toFloat())
+                binding.view4.animate().setDuration(ANIM_DURATION).rotation(backDegree.toFloat())
+                handleBackButtons()
+            }
+            R.id.iv_end_down -> {
+                backDegree -= 10
+                binding.view3.animate().setDuration(ANIM_DURATION).rotation(-backDegree.toFloat())
+                binding.view4.animate().setDuration(ANIM_DURATION).rotation(backDegree.toFloat())
+                handleBackButtons()
+            }
+        }
+    }
+
+    private fun handleBackButtons() {
+        binding.ivEndDown.isEnabled = backDegree > 0
+        binding.ivEndUp.isEnabled = backDegree < MAX_ROTATION
+    }
+
+    private fun handleFrontButtons() {
+        binding.ivFrontDown.isEnabled = frontDegree > 0
+        binding.ivFrontUp.isEnabled = frontDegree < MAX_ROTATION
+    }
+
+    companion object {
+        const val MAX_ROTATION = 45
+        const val ANIM_DURATION = 1500L
     }
 }
