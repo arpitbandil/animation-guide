6beb72608b8f58842862d4b6a236d6b6172d08dd
Arpit Bandil <arpit@goball.com>
2024-01-14 06:21:06 +0530

    Add Top cover

--- app/src/main/java/com/absoft/rotateanimation/MainActivity.kt
+++ app/src/main/java/com/absoft/rotateanimation/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.absoft.rotateanimation.databinding.ActivityMainBinding
 import com.absoft.rotateanimation.extensions.dp
-import com.absoft.rotateanimation.extensions.setMultiFunctionalTouchListener
-import com.absoft.rotateanimation.listeners.MultiFunctionalTouchListener
+import com.arpitbandil.multifunctionalbutton.MultiFunctionalTouchListener
+import com.arpitbandil.multifunctionalbutton.setMultiFunctionalTouchListener
 
 class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventListener,
     View.OnLayoutChangeListener {
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
 
     private var frontDegree = 0
     private var backDegree = 0
+    private var railDown = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,28 +34,44 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
         binding.ivFrontDown.setMultiFunctionalTouchListener(this)
         binding.ivEndUp.setMultiFunctionalTouchListener(this)
         binding.ivEndDown.setMultiFunctionalTouchListener(this)
+        binding.ivSideRail.setMultiFunctionalTouchListener(this)
+        binding.ivRailUp.setMultiFunctionalTouchListener(this)
+        binding.ivRailDown.setMultiFunctionalTouchListener(this)
+        binding.ivTopCover.setMultiFunctionalTouchListener(this)
+        binding.ivTopCover.addOnLayoutChangeListener(this)
+        binding.ivSideRail.addOnLayoutChangeListener(this)
+        binding.ivSideFrontRail.addOnLayoutChangeListener(this)
         binding.view1.addOnLayoutChangeListener(this)
         binding.view3.addOnLayoutChangeListener(this)
         binding.view4.addOnLayoutChangeListener(this)
     }
 
-    override fun onClick(v: View?) {
+    override fun onSingleClick(v: View?) {
         when (v?.id) {
             R.id.iv_front_up -> {
                 frontDegree =
-                    if (frontDegree + 10 > MAX_ROTATION) MAX_ROTATION else frontDegree + 10
+                    if (frontDegree + 10 > MAX_FRONT_ROTATION) MAX_FRONT_ROTATION else frontDegree + 10
                 binding.view1.animate().setDuration(ANIM_DURATION).rotation(frontDegree.toFloat())
+                binding.ivSideRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
                 handleFrontButtons()
             }
 
             R.id.iv_front_down -> {
                 frontDegree = if (frontDegree - 10 < 0) 0 else frontDegree - 10
                 binding.view1.animate().setDuration(ANIM_DURATION).rotation(frontDegree.toFloat())
+                binding.ivSideRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
                 handleFrontButtons()
             }
 
             R.id.iv_end_up -> {
-                backDegree = if (backDegree + 10 > MAX_ROTATION) MAX_ROTATION else backDegree + 10
+                backDegree =
+                    if (backDegree + 10 > MAX_BACK_ROTATION) MAX_BACK_ROTATION else backDegree + 10
                 binding.view3.animate().setDuration(ANIM_DURATION).rotation(-backDegree.toFloat())
                 binding.view4.animate().setDuration(ANIM_DURATION)
                     .translationX(-backDegree.toFloat().dp).rotation(backDegree.toFloat())
@@ -68,15 +85,47 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
                     .translationX(-backDegree.toFloat().dp).rotation(backDegree.toFloat())
                 handleBackButtons()
             }
+
+            R.id.iv_side_rail -> {
+                v.let {
+                    it.pivotY = it.height.toFloat()
+                    it.animate().setDuration(ANIM_DURATION)
+                        .scaleY(if (it.scaleY <= 0.4f) 1f else 0.4f)
+                    it.pivotY = it.height.toFloat() / 2
+                }
+            }
+
+            R.id.iv_top_cover -> {
+                v.animate().setDuration(ANIM_DURATION)
+                    .scaleX(if (v.scaleX <= 0.2f) 1f else 0.2f)
+            }
+
+            R.id.iv_rail_up -> {
+                railDown = if (railDown - 10 < 0) 0 else railDown - 10
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .translationY(railDown.toFloat())
+                handleRailButtons()
+            }
+
+            R.id.iv_rail_down -> {
+                railDown = if (railDown + 10 > MAX_RAIL_DOWN) MAX_RAIL_DOWN else railDown + 10
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .translationY(railDown.toFloat())
+                handleRailButtons()
+            }
         }
     }
 
     override fun onHolding(v: View?) {
         when (v?.id) {
             R.id.iv_front_up -> {
-                if (frontDegree < MAX_ROTATION) {
+                if (frontDegree < MAX_FRONT_ROTATION) {
                     frontDegree += 1
                     binding.view1.animate().setDuration(HOLD_ANIM_DURATION)
+                        .rotation(frontDegree.toFloat())
+                    binding.ivSideRail.animate().setDuration(HOLD_ANIM_DURATION)
+                        .rotation(frontDegree.toFloat())
+                    binding.ivSideFrontRail.animate().setDuration(HOLD_ANIM_DURATION)
                         .rotation(frontDegree.toFloat())
                     binding.tvFrontDegree.animate().setDuration(HOLD_ANIM_DURATION)
                         .rotation(-frontDegree.toFloat())
@@ -89,6 +138,10 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
                     frontDegree -= 1
                     binding.view1.animate().setDuration(HOLD_ANIM_DURATION)
                         .rotation(frontDegree.toFloat())
+                    binding.ivSideRail.animate().setDuration(HOLD_ANIM_DURATION)
+                        .rotation(frontDegree.toFloat())
+                    binding.ivSideFrontRail.animate().setDuration(HOLD_ANIM_DURATION)
+                        .rotation(frontDegree.toFloat())
                     binding.tvFrontDegree.animate().setDuration(HOLD_ANIM_DURATION)
                         .rotation(-frontDegree.toFloat())
                     handleFrontButtons()
@@ -96,7 +149,7 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
             }
 
             R.id.iv_end_up -> {
-                if (backDegree < MAX_ROTATION) {
+                if (backDegree < MAX_BACK_ROTATION) {
                     backDegree += 1
                     binding.view3.animate().setDuration(HOLD_ANIM_DURATION)
                         .rotation(-backDegree.toFloat())
@@ -118,26 +171,54 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
                     handleBackButtons()
                 }
             }
+
+            R.id.iv_rail_up -> {
+                if (railDown > 0) {
+                    railDown -= 1
+                    binding.ivSideFrontRail.animate().setDuration(HOLD_ANIM_DURATION)
+                        .translationY(railDown.toFloat())
+                    handleRailButtons()
+                }
+            }
+
+            R.id.iv_rail_down -> {
+                if (railDown < MAX_RAIL_DOWN) {
+                    railDown += 1
+                    binding.ivSideFrontRail.animate().setDuration(HOLD_ANIM_DURATION)
+                        .translationY(railDown.toFloat())
+                    handleRailButtons()
+                }
+            }
         }
-        v?.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+        if (v?.id != R.id.iv_side_rail && v?.id != R.id.iv_top_cover) {
+            v?.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+        }
     }
 
     override fun onDoubleClick(v: View?) {
         when (v?.id) {
             R.id.iv_front_up -> {
-                frontDegree = MAX_ROTATION
+                frontDegree = MAX_FRONT_ROTATION
                 binding.view1.animate().setDuration(ANIM_DURATION).rotation(frontDegree.toFloat())
+                binding.ivSideRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
                 handleFrontButtons()
             }
 
             R.id.iv_front_down -> {
                 frontDegree = 0
                 binding.view1.animate().setDuration(ANIM_DURATION).rotation(frontDegree.toFloat())
+                binding.ivSideRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .rotation(frontDegree.toFloat())
                 handleFrontButtons()
             }
 
             R.id.iv_end_up -> {
-                backDegree = MAX_ROTATION
+                backDegree = MAX_BACK_ROTATION
                 binding.view3.animate().setDuration(ANIM_DURATION).rotation(-backDegree.toFloat())
                 binding.view4.animate().setDuration(ANIM_DURATION)
                     .translationX(-backDegree.toFloat().dp).rotation(backDegree.toFloat())
@@ -150,6 +231,20 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
                 binding.view4.animate().setDuration(ANIM_DURATION)
                     .translationX(-backDegree.toFloat().dp).rotation(backDegree.toFloat())
                 handleBackButtons()
+            }
+
+            R.id.iv_rail_up -> {
+                railDown = 0
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .translationY(railDown.toFloat())
+                handleRailButtons()
+            }
+
+            R.id.iv_rail_down -> {
+                railDown = MAX_RAIL_DOWN
+                binding.ivSideFrontRail.animate().setDuration(ANIM_DURATION)
+                    .translationY(railDown.toFloat())
+                handleRailButtons()
             }
         }
     }
@@ -168,15 +263,20 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
         super.onHoldEnd(v)
     }
 
+    private fun handleRailButtons() {
+        binding.ivRailUp.isEnabled = railDown > 0
+        binding.ivRailDown.isEnabled = railDown < MAX_RAIL_DOWN
+    }
+
     private fun handleBackButtons() {
         binding.ivEndDown.isEnabled = backDegree > 0
-        binding.ivEndUp.isEnabled = backDegree < MAX_ROTATION
+        binding.ivEndUp.isEnabled = backDegree < MAX_BACK_ROTATION
     }
 
     private fun handleFrontButtons() {
         binding.tvFrontDegree.text = frontDegree.toString()
         binding.ivFrontDown.isEnabled = frontDegree > 0
-        binding.ivFrontUp.isEnabled = frontDegree < MAX_ROTATION
+        binding.ivFrontUp.isEnabled = frontDegree < MAX_FRONT_ROTATION
     }
 
     override fun onLayoutChange(
@@ -192,7 +292,9 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
     ) {
         view?.let {
             when (it.id) {
-                R.id.view1, R.id.view4 -> it.pivotX = it.width.toFloat()
+                R.id.view1, R.id.view4, R.id.iv_top_cover,
+                R.id.iv_side_rail, R.id.iv_side_front_rail -> it.pivotX = it.width.toFloat()
+
                 R.id.view3 -> view.pivotX = 0f
             }
             it.pivotY = it.height / 2f
@@ -200,7 +302,9 @@ class MainActivity : AppCompatActivity(), MultiFunctionalTouchListener.EventList
     }
 
     companion object {
-        const val MAX_ROTATION = 60
+        const val MAX_FRONT_ROTATION = 70
+        const val MAX_BACK_ROTATION = 35
+        const val MAX_RAIL_DOWN = 80
         const val ANIM_DURATION = 1500L
         const val HOLD_ANIM_DURATION = 150L
     }
--- app/src/main/java/com/absoft/rotateanimation/extensions/Extension.kt
+++ app/src/main/java/com/absoft/rotateanimation/extensions/Extension.kt
@@ -1,12 +1,6 @@
 package com.absoft.rotateanimation.extensions
 
 import android.content.res.Resources
-import android.view.View
-import com.absoft.rotateanimation.listeners.MultiFunctionalTouchListener
-
-fun View.setMultiFunctionalTouchListener(eventListener: MultiFunctionalTouchListener.EventListener) {
-    setOnTouchListener(MultiFunctionalTouchListener(this, eventListener))
-}
 
 val Float.dp: Float
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f)
